0b4cb980ceb232c2d906684654ed3152462e1e20
Viero De Popoda <tr0p6661@gmail.com>
2020-07-13 03:33:48 +0300

    .

--- TimePlanner/Models/BaseModel.cs
+++ TimePlanner/Models/BaseModel.cs
@@ -3,8 +3,9 @@ using System.ComponentModel.DataAnnotations;
 
 namespace TimePlanner.Models
 {
-    public class BaseModel // базовая модель для всех будущих моделей
+    public class BaseModel     
     {
+        // базовая модель для всех будущих моделей
         [Key]
         public Guid Id { get; set; }
     }
--- TimePlanner/wwwroot/js/site.js
+++ TimePlanner/wwwroot/js/site.js
@@ -1,7 +1,6 @@
 ﻿/// <reference path="../lib/signalr/dist/browser/signalr.js" />
 // Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
-
 // Write your JavaScript code.
 
 
